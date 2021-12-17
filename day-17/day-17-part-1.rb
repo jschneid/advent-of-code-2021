1bbd8821e6f64a779a7d1ba54771dcf1a1d3a92e
Jon Schneider <jon.schneider@appfolio.com>
2021-12-17 09:24:31 -0500

    Generalize solution and improve explanation
    ...To reflect my better understanding of the math, following my completion of Part 2.

--- day-17/day-17-part-1.rb
+++ day-17/day-17-part-1.rb
@@ -16,17 +16,15 @@ def max_y_if_trajectory_intersects_target(initial_y_velocity, y_target_area)
   nil
 end
 
-# This is admittedly a bit of a sloppy solution that relies on a pair of assumptions:
+# This solution assumes that the target area is defined such that:
 #
 # (1) The curve intersecting the target area with the highest max_y is going to be one
 # where the x velocity has been reduced to 0 (i.e. the probe is in free-fall); so we
 # can ignore the x dimension entirely for this part!
 #
-# (2) The math works out such that once we get beyond some particular not-too-large
-# value for initial_y_velocity, there won't be any more solutions -- any higher values
-# will cause the probe to fall past the target area without entering it on any step.
-def guess_highest_y_trajectory(y_target_area)
-  200.downto(1) do |initial_y_velocity|
+# (2) The area is entirely in the negative-y range.
+def find_highest_y_trajectory(y_target_area)
+  (y_target_area.min * -1).downto(1) do |initial_y_velocity|
     max_y = max_y_if_trajectory_intersects_target(initial_y_velocity, y_target_area)
     return max_y unless max_y.nil?
   end
@@ -34,4 +32,4 @@ def guess_highest_y_trajectory(y_target_area)
 end
 
 y_target_area = read_y_target_area
-p guess_highest_y_trajectory(y_target_area)
+p find_highest_y_trajectory(y_target_area)
